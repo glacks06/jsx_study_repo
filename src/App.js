dd24bd34cccd5fe466a740948a5f4760cf0b85ed
glacks06(mac) <a91125149@gmail.com>
2025-08-26 15:50:05 +0900

    jsx variable 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-
+const box1_name = 'box1: variable test';
 
 function App() {
   return (
@@ -10,7 +10,11 @@ function App() {
         <h1>React JSX Study</h1>
         <hr />
       </header>
+      <section>
+        {box1_name}
+      </section>
     </div>
+
   );
 }
 
