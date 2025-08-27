ba1569a045b649ad705c79bf943371a11ddf5149
glacks06(mac) <a91125149@gmail.com>
2025-08-28 06:36:34 +0900

    ToggleSwitch component 별도 js 파일로 관리 -> button.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from "react";
+import ToggleSwitch from './button';
 
 const box1_name = 'box1: variable test';
 
@@ -18,32 +19,6 @@ function PrintText(props){
   return <h3>text = {props.text}</h3>
 }
 
-function ToggleSwitch() {
-  // `isOn`이라는 상태(state)를 정의하고 초기값을 `false`(꺼짐)로 설정합니다.
-  const [isOn, setIsOn] = useState(false);
-
-  // 버튼 클릭 시 상태를 반전시키는 함수
-  const toggleState = () => {
-    setIsOn(!isOn);
-  };
-
-  const switchStyle = {
-    padding: '10px 20px',
-    backgroundColor: isOn ? '#4CAF50' : '#f44336', // 상태에 따라 색상 변경
-    color: 'white',
-    borderRadius: '5px',
-    cursor: 'pointer',
-    transition: 'background-color 0.3s'
-  };
-
-  return (
-    <div style={{ margin: '10px' }}>
-      <button onClick={toggleState} style={switchStyle}>
-        {isOn ? '불 켜짐' : '불 꺼짐'}
-      </button>
-    </div>
-  );
-}
 
 function App() {
   return (
---
+++ src/button.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from "react";
+
+function ToggleSwitch() {
+  // `isOn`이라는 상태(state)를 정의하고 초기값을 `false`(꺼짐)로 설정합니다.
+  const [isOn, setIsOn] = useState(false);
+
+  // 버튼 클릭 시 상태를 반전시키는 함수
+  const toggleState = () => {
+    setIsOn(!isOn);
+  };
+
+  const switchStyle = {
+    padding: '10px 20px',
+    backgroundColor: isOn ? '#4CAF50' : '#f44336', // 상태에 따라 색상 변경
+    color: 'white',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s'
+  };
+
+  return (
+    <div style={{ margin: '10px' }}>
+      <button onClick={toggleState} style={switchStyle}>
+        {isOn ? '불 켜짐' : '불 꺼짐'}
+      </button>
+    </div>
+  );
+}
+
+export default ToggleSwitch;
