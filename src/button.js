58b1c2d9ec236a1d664f6eac5af45bf5e2135b8f
glacks06(mac) <a91125149@gmail.com>
2025-08-29 22:35:18 +0900

    button.js -> component 네이밍 주의사항 관련 주석 작성

--- src/button.js
+++ src/button.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 
+// 컴포넌트 네이밍 시 첫글자는 반드시 대문자
 function ToggleSwitch() {
   // `isOn`이라는 상태(state)를 정의하고 초기값을 `false`(꺼짐)로 설정합니다.
   const [isOn, setIsOn] = useState(false);
